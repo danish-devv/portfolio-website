36628b6913871e5512b03be9eb89a417c274a9bc
danish <danishmr835@gmail.com>
2026-06-28 21:35:19 +0500

    setup cors and axios instance

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,23 @@
-import express from "express"
-import contactRoute from "./routes/contactRoute.js"
-import { errorMiddleware } from "./middlewares/errorMiddleware.js"
-const app=express()
+import express from "express";
+import cors from "cors";
+import contactRoute from "./routes/contactRoute.js";
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 
-app.use(express.json())
+const app = express();
 
-app.use("/contact",contactRoute)
-app.use(errorMiddleware)
-export default app
+app.use(
+  cors({
+    origin: [
+      "https://portfolio-website-tan-two-17.vercel.app",
+      "http://localhost:5173",
+    ],
+  }),
+);
+
+app.use(express.json());
+
+app.use("/contact", contactRoute);
+
+app.use(errorMiddleware);
+
+export default app;
